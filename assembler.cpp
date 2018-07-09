d278f4f948c9dbf71a4f56fb2ee21c47c5b447e6
purpl3f0x <stavros9899@gmail.com>
2018-07-09 19:19:05 +0300

    update coding format

--- assembler.cpp
+++ assembler.cpp
@@ -4,419 +4,408 @@
 
 #include "assembler.hpp"
 
-
 namespace asmbl {
-
 
 Assembler::Assembler(bool mode = false) : onDebug{mode} {
 
-    //Basic operationns
-    opCodes["NOP"] = 0b0000001;
-    opCodes["LDA"] = 0b0000010;
-    opCodes["LDI"] = 0b0000011;
-    opCodes["STA"] = 0b0000100;
-    opCodes["JMP"] = 0b0000101;
-    opCodes["MI"] = 0b0000110;
-    opCodes["MO"] = 0b0000111;
+  //Basic operationns
+  opCodes["NOP"] = 0b0000001;
+  opCodes["LDA"] = 0b0000010;
+  opCodes["LDI"] = 0b0000011;
+  opCodes["STA"] = 0b0000100;
+  opCodes["JMP"] = 0b0000101;
+  opCodes["MI"] = 0b0000110;
+  opCodes["MO"] = 0b0000111;
 
 
-    //Conditional Jumps
-    opCodes["JC"] = 0b0001000;
-    opCodes["JO"] = 0b0001001;
-    opCodes["JG"] = 0b0001010;
-    opCodes["JGE"] = 0b0001011;
-    opCodes["JE"] = 0b0001100;
-    opCodes["JLE"] = 0b0001101;
-    opCodes["JL"] = 0b0001110;
-    opCodes["JNO"] = 0b0001111;
-    opCodes["JZ"] = 0b0010000;
-    opCodes["JNZ"] = 0b0010001;
-    opCodes["JS"] = 0b0010010;
-    opCodes["JNS"] = 0b0010011;
+  //Conditional Jumps
+  opCodes["JC"] = 0b0001000;
+  opCodes["JO"] = 0b0001001;
+  opCodes["JG"] = 0b0001010;
+  opCodes["JGE"] = 0b0001011;
+  opCodes["JE"] = 0b0001100;
+  opCodes["JLE"] = 0b0001101;
+  opCodes["JL"] = 0b0001110;
+  opCodes["JNO"] = 0b0001111;
+  opCodes["JZ"] = 0b0010000;
+  opCodes["JNZ"] = 0b0010001;
+  opCodes["JS"] = 0b0010010;
+  opCodes["JNS"] = 0b0010011;
 
-    // ALU
-    opCodes["ADD"] = 0b1000000;
-    opCodes["SUB"] = 0b1000001;
-    opCodes["MUL"] = 0b1000010;
-    opCodes["DIV"] = 0b1000011;
-    opCodes["ADDI"] = 0b1000100;
-    opCodes["SUBI"] = 0b1000101;
-    opCodes["MULI"] = 0b1000110;
-    opCodes["DIVI"] = 0b1000111;
-    //Complex ALU operations
-    opCodes["SQRT"] = 0b1001000;
-    opCodes["MOD"] = 0b1001001;
+  // ALU
+  opCodes["ADD"] = 0b1000000;
+  opCodes["SUB"] = 0b1000001;
+  opCodes["MUL"] = 0b1000010;
+  opCodes["DIV"] = 0b1000011;
+  opCodes["ADDI"] = 0b1000100;
+  opCodes["SUBI"] = 0b1000101;
+  opCodes["MULI"] = 0b1000110;
+  opCodes["DIVI"] = 0b1000111;
+  //Complex ALU operations
+  opCodes["SQRT"] = 0b1001000;
+  opCodes["MOD"] = 0b1001001;
 
-    // Bitwise operationns
-    opCodes["AND"] = 0b1100000;
-    opCodes["OR"] = 0b11000001;
-    opCodes["NAND"] = 0b1100010;
-    opCodes["NOR"] = 0b1100011;
-    opCodes["NOT"] = 0b1100100;
-    opCodes["XOR"] = 0b1100101;
-    opCodes["XOR"] = 0b1100110;
-    opCodes["SHL"] = 0b1100111;
-    opCodes["SHR"] = 0b1101000;
-    opCodes["ROT"] = 0b1101001;
-    opCodes["CMP"] = 0b1101010;
+  // Bitwise operationns
+  opCodes["AND"] = 0b1100000;
+  opCodes["OR"] = 0b11000001;
+  opCodes["NAND"] = 0b1100010;
+  opCodes["NOR"] = 0b1100011;
+  opCodes["NOT"] = 0b1100100;
+  opCodes["XOR"] = 0b1100101;
+  opCodes["XOR"] = 0b1100110;
+  opCodes["SHL"] = 0b1100111;
+  opCodes["SHR"] = 0b1101000;
+  opCodes["ROT"] = 0b1101001;
+  opCodes["CMP"] = 0b1101010;
 
-    //FPU
-    opCodes["FADD"] = 0b1110000;
-    opCodes["FSUB"] = 0b1110001;
-    opCodes["FMUL"] = 0b1110010;
-    opCodes["FDIV"] = 0b1110011;
-    opCodes["FSQRT"] = 0b1110100;
-    opCodes["ITOF"] = 0b1110101;
-    opCodes["FTOI"] = 0b1110111;
-    opCodes["FMOD"] = 0b1111000;
+  //FPU
+  opCodes["FADD"] = 0b1110000;
+  opCodes["FSUB"] = 0b1110001;
+  opCodes["FMUL"] = 0b1110010;
+  opCodes["FDIV"] = 0b1110011;
+  opCodes["FSQRT"] = 0b1110100;
+  opCodes["ITOF"] = 0b1110101;
+  opCodes["FTOI"] = 0b1110111;
+  opCodes["FMOD"] = 0b1111000;
 
-    //HACF -- HALT AND CATCH FIRE
-    opCodes["HLT"] = 0b1111111;
+  //HACF -- HALT AND CATCH FIRE
+  opCodes["HLT"] = 0b1111111;
 
-    //////////////////////////////////////////
-    /////// Number of Arguments of each opCode
+  //////////////////////////////////////////
+  /////// Number of Arguments of each opCode
 
-    //Basic operationns
-    numOfArgs["NOP"] = 0;
-    numOfArgs["LDA"] = 1;
-    numOfArgs["LDI"] = 1;
-    numOfArgs["JMP"] = 1;
-    numOfArgs["MI"] = 2;
-    numOfArgs["MO"] = 2;
+  //Basic operationns
+  numOfArgs["NOP"] = 0;
+  numOfArgs["LDA"] = 1;
+  numOfArgs["LDI"] = 1;
+  numOfArgs["JMP"] = 1;
+  numOfArgs["MI"] = 2;
+  numOfArgs["MO"] = 2;
 
-    //Conditional Jumps
-    numOfArgs["JC"] = 1;
-    numOfArgs["JO"] = 1;
-    numOfArgs["JG"] = 1;
-    numOfArgs["JGE"] = 1;
-    numOfArgs["JE"] = 1;
-    numOfArgs["JLE"] = 1;
-    numOfArgs["JL"] = 1;
-    numOfArgs["JNO"] = 1;
-    numOfArgs["JZ"] = 1;
-    numOfArgs["JNZ"] = 1;
-    numOfArgs["JS"] = 1;
-    numOfArgs["JNS"] = 1;
+  //Conditional Jumps
+  numOfArgs["JC"] = 1;
+  numOfArgs["JO"] = 1;
+  numOfArgs["JG"] = 1;
+  numOfArgs["JGE"] = 1;
+  numOfArgs["JE"] = 1;
+  numOfArgs["JLE"] = 1;
+  numOfArgs["JL"] = 1;
+  numOfArgs["JNO"] = 1;
+  numOfArgs["JZ"] = 1;
+  numOfArgs["JNZ"] = 1;
+  numOfArgs["JS"] = 1;
+  numOfArgs["JNS"] = 1;
 
-    // ALU
-    numOfArgs["ADD"] = 3;
-    numOfArgs["SUB"] = 3;
-    numOfArgs["MUL"] = 3;
-    numOfArgs["DIV"] = 3;
-    numOfArgs["ADDI"] = 3;
-    numOfArgs["SUBI"] = 3;
-    numOfArgs["MULI"] = 3;
-    numOfArgs["DIVI"] = 3;
-    //Complex ALU operations
-    numOfArgs["SQRT"] = 3;
-    numOfArgs["MOD"] = 3;
+  // ALU
+  numOfArgs["ADD"] = 3;
+  numOfArgs["SUB"] = 3;
+  numOfArgs["MUL"] = 3;
+  numOfArgs["DIV"] = 3;
+  numOfArgs["ADDI"] = 3;
+  numOfArgs["SUBI"] = 3;
+  numOfArgs["MULI"] = 3;
+  numOfArgs["DIVI"] = 3;
+  //Complex ALU operations
+  numOfArgs["SQRT"] = 3;
+  numOfArgs["MOD"] = 3;
 
-    // Bitwise operationns
-    numOfArgs["AND"] = 3;
-    numOfArgs["OR"] = 3;
-    numOfArgs["NAND"] = 3;
-    numOfArgs["NOR"] = 3;
-    numOfArgs["NOT"] = 3;
-    numOfArgs["XOR"] = 3;
-    numOfArgs["XOR"] = 3;
-    numOfArgs["SHL"] = 3;
-    numOfArgs["SHR"] = 3;
-    numOfArgs["ROT"] = 3;
+  // Bitwise operationns
+  numOfArgs["AND"] = 3;
+  numOfArgs["OR"] = 3;
+  numOfArgs["NAND"] = 3;
+  numOfArgs["NOR"] = 3;
+  numOfArgs["NOT"] = 3;
+  numOfArgs["XOR"] = 3;
+  numOfArgs["XOR"] = 3;
+  numOfArgs["SHL"] = 3;
+  numOfArgs["SHR"] = 3;
+  numOfArgs["ROT"] = 3;
 
-    //FPU
-    numOfArgs["FADD"] = 3;
-    numOfArgs["FSUB"] = 3;
-    numOfArgs["FMUL"] = 3;
-    numOfArgs["FDIV"] = 3;
-    numOfArgs["FSQRT"] = 3;
-    numOfArgs["ITOF"] = 3;
-    numOfArgs["FTOI"] = 3;
-    numOfArgs["FMOD"] = 3;
+  //FPU
+  numOfArgs["FADD"] = 3;
+  numOfArgs["FSUB"] = 3;
+  numOfArgs["FMUL"] = 3;
+  numOfArgs["FDIV"] = 3;
+  numOfArgs["FSQRT"] = 3;
+  numOfArgs["ITOF"] = 3;
+  numOfArgs["FTOI"] = 3;
+  numOfArgs["FMOD"] = 3;
 
-    //HACF -- HALT AND CATCH FIRE
-    numOfArgs["HLT"] = 0;
+  //HACF -- HALT AND CATCH FIRE
+  numOfArgs["HLT"] = 0;
 
 
-    ////////////////////////////////
-    //Registers
-    reg["r1"] = 0b000;
-    reg["r2"] = 0b001;
-    reg["r3"] = 0b010;
-    reg["r4"] = 0b011;
-    reg["r5"] = 0b100;
-    reg["r6"] = 0b101;
-    reg["r7"] = 0b110;
-    reg["r8"] = 0b111;
+  ////////////////////////////////
+  //Registers
+  reg["r1"] = 0b000;
+  reg["r2"] = 0b001;
+  reg["r3"] = 0b010;
+  reg["r4"] = 0b011;
+  reg["r5"] = 0b100;
+  reg["r6"] = 0b101;
+  reg["r7"] = 0b110;
+  reg["r8"] = 0b111;
 
 }
 
-
 int Assembler::opCode(string s) {
-    auto it = opCodes.find(s);
+  auto it = opCodes.find(s);
 
-    if (it != opCodes.end()) return it->second;
-    else throw logic_error("Got \" " + s + "\" expected an opcode");
+  if (it != opCodes.end()) return it->second;
+  else throw logic_error("Got \" " + s + "\" expected an opcode");
 }
 
 int Assembler::Register(string s) {
-    auto it = reg.find(s);
-    if (it != reg.end()) return it->second;
-    else throw logic_error(s + " is not a  register");
+  auto it = reg.find(s);
+  if (it != reg.end()) return it->second;
+  else throw logic_error(s + " is not a  register");
 }
 
 void Assembler::setInputFile(string infile) {
-    cur_file = infile;
+  cur_file = infile;
 }
 
 string tobinary(int n, int d = 16) {
-    string s;
-    while (d--) {
-        s = std::to_string(n % 2) + s;
-        n = n >> 1;
-    }
-    return s;
+  string s;
+  while (d--) {
+    s = std::to_string(n % 2) + s;
+    n = n >> 1;
+  }
+  return s;
 }
 
 /* Privare Methods */
 Assembler::instruction::instruction() : size{-1} {}
 
 void Assembler::instruction::add(std::string s) {
-    if (!++size) opCode = s;
-    else arguments.push_back(s);
+  if (!++size) opCode = s;
+  else arguments.push_back(s);
 }
 
 void Assembler::instruction::add(vector<std::string> &v) {
-    arguments.reserve(arguments.size() + v.size());       //Memory Pre allocation
-    arguments.insert(arguments.end(), v.begin(), v.end());
-    size += v.size();
+  arguments.reserve(arguments.size() + v.size());       //Memory Pre allocation
+  arguments.insert(arguments.end(), v.begin(), v.end());
+  size += v.size();
 }
 
 Assembler::error::error(int line, int index, string type = "", string message = "", string line_text = "")
-        : line{line}, index{index}, type{type}, message{message}, line_text{line_text} {
+    : line{line}, index{index}, type{type}, message{message}, line_text{line_text} {
 }
-
 
 const string Assembler::error::get() {
-    return type + " on line :" + to_string(line) + ":" + to_string(index) + "\t" + message;
+  return type + " on line :" + to_string(line) + ":" + to_string(index) + "\t" + message;
 }
 
-
 bool Assembler::isEmptyLine(const string &line) {
-    auto comment = x3::omit[
-            "//" >> *(char_ - eol)
-            | "/*" >> *(char_ - "*/") >> "*/"
-    ];
+  auto comment = x3::omit[
+      "//" >> *(char_ - eol)
+          | "/*" >> *(char_ - "*/") >> "*/"
+  ];
 
-    auto iter_start = line.begin();
-    auto iter_end = line.end();
+  auto iter_start = line.begin();
+  auto iter_end = line.end();
 
-    bool success = phrase_parse(
-            iter_start,
-            iter_end,
-            *comment,
-            space
-    );
+  bool success = phrase_parse(
+      iter_start,
+      iter_end,
+      *comment,
+      space
+  );
 
-    return success && (iter_end == iter_start);
+  return success && (iter_end == iter_start);
 }
 
 bool Assembler::isData(const string &line) {
-    auto comment = x3::omit[
-            "//" >> *(char_ - eol)
-            | "/*" >> *(char_ - "*/") >> "*/"
-    ];
-    auto iter_start = line.begin();
-    auto iter_end = line.end();
+  auto comment = x3::omit[
+      "//" >> *(char_ - eol)
+          | "/*" >> *(char_ - "*/") >> "*/"
+  ];
+  auto iter_start = line.begin();
+  auto iter_end = line.end();
 
-    auto data = x3::rule<class name, std::string>{}
-                        = lexeme["section "] >> lexeme[".data"];
+  auto data = x3::rule<class name, std::string>{}
+                  = lexeme["section "] >> lexeme[".data"];
 
-    bool success = parse(
-            iter_start,
-            iter_end,
-            skip(space)[data] >> *(space | comment)
-    );
+  bool success = parse(
+      iter_start,
+      iter_end,
+      skip(space)[data] >> *(space | comment)
+  );
 
-    return success && (iter_end == iter_start);
+  return success && (iter_end == iter_start);
 }
 
 bool Assembler::isText(const string &line) {
-    auto comment = x3::omit[
-            "//" >> *(char_ - eol)
-            | "/*" >> *(char_ - "*/") >> "*/"
-    ];
-    auto iter_start = line.begin();
-    auto iter_end = line.end();
+  auto comment = x3::omit[
+      "//" >> *(char_ - eol)
+          | "/*" >> *(char_ - "*/") >> "*/"
+  ];
+  auto iter_start = line.begin();
+  auto iter_end = line.end();
 
-    auto data = x3::rule<class name, std::string>{}
-                        = lexeme["section "] >> lexeme[".text"];
+  auto data = x3::rule<class name, std::string>{}
+                  = lexeme["section "] >> lexeme[".text"];
 
-    bool success = parse(
-            iter_start,
-            iter_end,
-            skip(space)[data] >> *(space | comment)
-    );
+  bool success = parse(
+      iter_start,
+      iter_end,
+      skip(space)[data] >> *(space | comment)
+  );
 
-    return success && (iter_end == iter_start);
+  return success && (iter_end == iter_start);
 }
 
 bool Assembler::isStart(const string &line) {
-    auto comment = x3::omit[
-            "//" >> *(char_ - eol)
-            | "/*" >> *(char_ - "*/") >> "*/"
-    ];
-    auto iter_start = line.begin();
-    auto iter_end = line.end();
+  auto comment = x3::omit[
+      "//" >> *(char_ - eol)
+          | "/*" >> *(char_ - "*/") >> "*/"
+  ];
+  auto iter_start = line.begin();
+  auto iter_end = line.end();
 
-    auto data = x3::rule<class name, std::string>{}
-                        = lexeme["section "] >> lexeme[".start"];
+  auto data = x3::rule<class name, std::string>{}
+                  = lexeme["section "] >> lexeme[".start"];
 
-    bool success = parse(
-            iter_start,
-            iter_end,
-            skip(space)[data] >> *(space | comment)
-    );
+  bool success = parse(
+      iter_start,
+      iter_end,
+      skip(space)[data] >> *(space | comment)
+  );
 
-    return success && (iter_end == iter_start);
+  return success && (iter_end == iter_start);
 }
 
+bool Assembler::lineParser(const string &line) {
 
-bool Assembler::lineParser(const string line) {
+  // Define parser rules
+  auto name = x3::rule<class name, std::string>{}
+                  = lexeme[char_("a-zA-Z") >> *char_("a-z_A-Z0-9")];
 
-    // Define parser rules
-    auto name = x3::rule<class name, std::string>{}
-                        = lexeme[char_("a-zA-Z") >> *char_("a-z_A-Z0-9")];
+  auto bin_val = x3::rule<class bin_val, std::string>{}
+                     = lexeme[char_("0b") >> +char_("0-1")];
 
-    auto bin_val = x3::rule<class bin_val, std::string>{}
-                           = lexeme[char_("0x") >> +char_("0-1")];
+  auto hex_val = x3::rule<class hex_val, std::string>{}
+                     = lexeme[char_("0x") >> +char_("0-9A-F")];
 
-    auto hex_val = x3::rule<class hex_val, std::string>{}
-                           = lexeme[char_("0x") >> +char_("0-9A-F")];
+  auto comment = x3::omit[
+      "//" >> *(char_ - eol)
+          | "/*" >> *(char_ - "*/") >> "*/"
+  ];
 
+  //string iterators
+  auto iter_start = line.begin();
+  auto iter_end = line.end();
 
-    auto comment = x3::omit[
-            "//" >> *(char_ - eol)
-            | "/*" >> *(char_ - "*/") >> "*/"
-    ];
+  instruction inst;
 
-    //string iterators
-    auto iter_start = line.begin();
-    auto iter_end = line.end();
+  // Bind
+  auto add = [&](auto &ctx) { inst.add(_attr(ctx)); };
 
-    instruction inst;
+  bool result = parse(
+      iter_start,
+      iter_end,
+      skip(comment | space)[name[add] >> *(name[add] % ',')] >> *(space | comment)
+  );
 
-    // Bind
-    auto add = [&](auto &ctx) { inst.add(_attr(ctx)); };
+  //Testing
 
-    bool result = parse(
-            iter_start,
-            iter_end,
-            skip(comment | space)[name[add] >> *(name[add] % ',')] >> *(space | comment)
+  if (result && inst.size && opCodes.find(inst.opCode) == opCodes.end()) {    // given identifier is not valid opCode
+    result = false;
+
+    errors.emplace_back(
+        error(cur_line,
+              iter_end - iter_start,
+              "Syntax Error",
+              inst.opCode + " is not a valid opcode.",    // Example: 42 is no a valid opcode
+              line
+        )
     );
+  } else if (result && inst.size != numOfArgs[inst.opCode]) {  // Check if have right number of arguments
 
-    //Testing
+    result = false;
 
-    if (result && inst.size && opCodes.find(inst.opCode) == opCodes.end()) {    // given identifier is not valid opCode
-        result = false;
+    errors.emplace_back(
+        error(cur_line,
+              iter_start - line.begin(),
+              "Logic Error",
+              inst.opCode + " expects " + to_string(numOfArgs[inst.opCode]) + " argument(s), got " +
+                  to_string(inst.size) + ".",    // Example: ADD expects 3 arguments, got 4
+              line
+        )
+    );
+  }
 
-        errors.emplace_back(
-                error(cur_line,
-                      iter_end - iter_start,
-                      "Syntax Error",
-                      inst.opCode + " is not a valid opcode.",    // Example: 42 is no a valid opcode
-                      line
-                )
-        );
-    } else if (result && inst.size != numOfArgs[inst.opCode]) {  // Check if have right number of arguments
+  if (iter_end != iter_start) {   // Create an error if parser
 
-        result = false;
+    result = false;
 
-        errors.emplace_back(
-                error(cur_line,
-                      iter_start - line.begin(),
-                      "Logic Error",
-                      inst.opCode + " expects " + to_string(numOfArgs[inst.opCode]) + " argument(s), got " +
-                      to_string(inst.size) + ".",    // Example: ADD expects 3 arguments, got 4
-                      line
-                )
-        );
-    }
+    errors.emplace_back(
+        error(cur_line,
+              iter_start - line.begin(),
+              "Syntax Error",
+              "Unexpected -> " + string(iter_start, iter_end),
+              line
+        )
+    );
+  }
 
-    if (iter_end != iter_start) {   // Create an error if parser
+  // Code used for debugging
+  if (onDebug) {
+    cout << cur_line << ": " << inst.opCode << "(" << numOfArgs[inst.opCode] << ")" << endl;
 
-        result = false;
+    for (auto i : inst.arguments)
+      cout << "\t->" << i << std::endl;
 
-        errors.emplace_back(
-                error(cur_line,
-                      iter_start - line.begin(),
-                      "Syntax Error",
-                      "Unexpected -> " + string(iter_start, iter_end),
-                      line
-                )
-        );
-    }
+    if (!result)
+      for (auto e : errors)
+        cout << e.get() << endl;
+  }
 
-    // Code used for debugging
-    if (onDebug) {
-        cout << cur_line << ": " << inst.opCode << "(" << numOfArgs[inst.opCode] << ")" << endl;
+  //
 
-        for (auto i : inst.arguments)
-            cout << "\t->" << i << std::endl;
-
-        if (!result)
-            for (auto e : errors)
-                cout << e.get() << endl;
-    }
-
-    //
-
-    if (result) instructions.push_back(inst);
-    return result;
+  if (result) instructions.push_back(inst);
+  return result;
 }
 
 bool Assembler::parser(const string &s) {
-    cur_line = 0;
+  cur_line = 0;
 
-    stringstream stream(s);
-    string line;
-    while (std::getline(stream, line)) {
-        cur_line++;
-        lineParser(line);
-    }
+  stringstream stream(s);
+  string line;
+  while (std::getline(stream, line)) {
+    cur_line++;
+    lineParser(line);
+  }
 
 }
 
 bool Assembler::parser(ifstream &fs) {
 
+  cur_line = 0;
+  string line;
+  bool success = true;
 
-    cur_line = 0;
-    string line;
-    bool success = true;
+  while (std::getline(fs, line)) {
 
-    while (std::getline(fs, line)) {
+    cur_line++;
 
-        cur_line++;
+    if (isEmptyLine(line)) continue;      //skip empty lines
 
-        if (isEmptyLine(line)) continue;      //skip empty lines
-
-        if (isData(line)) {
-            continue;
-        }
-        if (isText(line)) {
-            continue;
-        }
-        if (isStart(line)) {
-            continue;
-        }
-
-
-
-        success =
-                lineParser(line)
-                && success;
-
+    if (isData(line)) {
+      continue;
     }
-}
+    if (isText(line)) {
+      continue;
+    }
+    if (isStart(line)) {
+      continue;
+    }
 
+    success =
+        lineParser(line)
+            && success;
+
+  }
+}
 
 } //End of asmbl namesapce
--- assembler.hpp
+++ assembler.hpp
@@ -7,7 +7,6 @@
 #ifndef UNTITLED_ASSEMBLER_HPP
 #define UNTITLED_ASSEMBLER_HPP
 
-
 #include <boost/fusion/adapted.hpp>
 #include <boost/spirit/home/x3.hpp>
 
@@ -22,9 +21,7 @@
 #include <string>
 #include <vector>
 
-
 namespace asmbl {
-
 
 using namespace std;
 
@@ -45,94 +42,89 @@ using x3::skip;
 
 using ascii::space;
 
-
 class Assembler {
 
-public:
-    Assembler(bool mode);
+ public:
+ Assembler(bool mode);
 
-    int opCode(string s);
+ int opCode(string s);
 
-    int Register(string s);
+ int Register(string s);
 
-    void setInputFile(string file);
+ void setInputFile(string file);
 
-    bool parseString(const string &s) { return parser(s); }
+ bool parseString(const string &s) { return parser(s); }
 
-    bool parseFile() {
-        ifstream infile;
-        infile.open(cur_file);
-        if (onDebug) {
-            cout << "Opening " << cur_file << " ..." << endl;
-            if (infile) cout << "Ok\n\n";
-            else cout << "Cannot Open File\n\n";
-        }
-        return parser(infile);
-    }
+ bool parseFile() {
+   ifstream infile;
+   infile.open(cur_file);
+   if (onDebug) {
+     cout << "Opening " << cur_file << " ..." << endl;
+     if (infile) cout << "Ok\n\n";
+     else cout << "Cannot Open File\n\n";
+   }
+   return parser(infile);
+ }
 
-    void translate();
+ void translate();
 
-    void printErrors();
+ void printErrors();
 
-    friend string tobinary(int, int);
+ friend string tobinary(int, int);
 
+ private:
+ struct instruction {
+   string opCode;
+   vector<string> arguments;
+   int size;
 
-private:
-    struct instruction {
-        string opCode;
-        vector<string> arguments;
-        int size;
+   instruction();
 
-        instruction();
+   void add(string s);
 
-        void add(string s);
+   void add(vector<std::string> &v);
+ };
 
-        void add(vector<std::string> &v);
-    };
+ struct error {
+   int line;
+   int index;
+   string type;
+   string message;
+   string line_text;
 
+   error(int line, int index, string type, string message, string line_text);
 
-    struct error {
-        int line;
-        int index;
-        string type;
-        string message;
-        string line_text;
+   const string get();
+ };
 
-        error(int line, int index, string type, string message, string line_text);
+ int cur_line;
+ string cur_file;
+ bool onDebug;
 
-        const string get();
-    };
+ // All maps provιde binary value of each type
+ map<string, int> opCodes;       // List off all opCodes
+ map<string, int> numOfArgs;     // Number of Arguments of each opCode
+ map<string, int> reg;           // Registers
+ map<string, int> variables;     // Variable Stack
+ map<string, int> consts;        // Const Stack
 
+ vector<instruction> instructions;
+ vector<error> errors;
+ vector<string> out;
 
-    int cur_line;
-    string cur_file;
-    bool onDebug;
+ bool isEmptyLine(const string &line);
 
-    // All maps provιde binary value of each type
-    map<string, int> opCodes;       // List off all opCodes
-    map<string, int> numOfArgs;     // Number of Arguments of each opCode
-    map<string, int> reg;           // Registers
-    map<string, int> variables;     // Variable Stack
-    map<string, int> consts;        // Const Stack
+ bool isData(const string &line);
 
-    vector<instruction> instructions;
-    vector<error> errors;
-    vector<string> out;
+ bool isText(const string &line);
 
-    bool isEmptyLine(const string &line);
+ bool isStart(const string &line);
 
-    bool isData(const string &line);
+ bool lineParser(const string &line);
 
-    bool isText(const string &line);
+ bool parser(const string &s);
 
-    bool isStart(const string &line);
-
-    bool lineParser(const string line);
-
-    bool parser(const string &s);
-
-    bool parser(ifstream &fs);
-
+ bool parser(ifstream &fs);
 
 };
 
